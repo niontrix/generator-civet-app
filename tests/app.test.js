87ec534364912b3856da26c48b0f059bbbe1ed01
Tobias Heinlein <dev_th@mailbox.org>
2026-08-13 10:12:09 +0200

    tests(farm): add tests for farm framework

--- tests/app.test.js
+++ tests/app.test.js
@@ -31,3 +31,24 @@ describe("generator-civet-app:app - esbuild", () => {
     expect(fs.readFileSync(`${appName}/package.json`, "utf8")).toContain("esbuild");
   });
 });
+
+describe("generator-civet-app:app - farm", () => {
+  beforeAll(() => helpers
+    .run(path.join(__dirname, "../generators/app"))
+    .withPrompts({ appName, buildFramework: "farm" }));
+
+  it("creates files", () => {
+    expect(fs.existsSync(`${appName}/farm.config.js`)).toBe(true);
+    expect(fs.existsSync(`${appName}/package.json`)).toBe(true);
+    expect(fs.existsSync(`${appName}/tsconfig.json`)).toBe(true);
+    expect(fs.existsSync(`${appName}/src/main.civet`)).toBe(true);
+    expect(fs.existsSync(`${appName}/src/style.css`)).toBe(true);
+    expect(fs.existsSync(`${appName}/.gitignore`)).toBe(true);
+    expect(fs.existsSync(`${appName}/.vscode/launch.json`)).toBe(true);
+    expect(fs.existsSync(`${appName}/.vscode/tasks.json`)).toBe(true);
+  });
+
+  it("adds farm dependencies", () => {
+    expect(fs.readFileSync(`${appName}/package.json`, "utf8")).toContain("farm");
+  });
+});
